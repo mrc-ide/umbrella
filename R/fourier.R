3574c412c2aae223810d938129af8b4d451af58e
Peter Winskill <p.winskill@imperial.ac.uk>
2022-07-29 10:03:02 +0100

    Removing rgee and google earth engine dependency

--- R/fourier.R
+++ R/fourier.R
@@ -3,6 +3,10 @@
 #' @inheritParams objective
 #' @export
 fourier_predict <- function(coef, t, floor){
+  if(any(t > 365) | any (t < 1)){
+    stop("t must be between 1 and 365")
+  }
+  t <- t / 365
   coef <- unname(coef)
   prediction <- data.frame(
     g0 = coef[1],
@@ -11,10 +15,10 @@ fourier_predict <- function(coef, t, floor){
     g3 = coef[4] * basecos(level = 3, t = t),
     h1 = coef[5] * basesin(level = 1, t = t),
     h2 = coef[6] * basesin(level = 2, t = t),
-    h3 = coef[7] * basesin(level = 3, t = t)) %>%
-    rowSums()
+    h3 = coef[7] * basesin(level = 3, t = t))
+  prediction <- rowSums(prediction)
   prediction <- pmax(floor, prediction)
-  out <- data.frame(t = t, profile = prediction)
+  out <- data.frame(t = t * 365, profile = prediction)
   return(out)
 }
 
@@ -25,7 +29,7 @@ fourier_predict <- function(coef, t, floor){
 #'
 #' @return Sum of squared differences between profile and rainfall data
 objective <- function(coef, t, floor, rainfall){
-  sum((fourier_predict(coef = coef, t = t, floor = floor)$profile - rainfall)^2)
+  sum((umbrella::fourier_predict(coef = c(mean(rainfall), coef), t = t, floor = floor)$profile - rainfall)^2)
 }
 
 #' Fit fourier parameters
@@ -34,25 +38,61 @@ objective <- function(coef, t, floor, rainfall){
 #' with `lm()`.
 #'
 #' @param rainfall Vector of rainfall
-#' @param t Vector of timesteps (between 0 and 1)
+#' @param t Vector of days (between 1 and 365)
 #' @param floor Lower bound on rainfall fit
 #'
 #' @return Model fit
 #' @export
 fit_fourier <- function(rainfall, t, floor = 0.001){
-  fit <- stats::nlm(f = objective, p = c(mean(rainfall), rep(0, 6)), t = t, rainfall = rainfall, floor = floor)
-  coefficients <- fit$estimate
-  fit$coefficients <- as.numeric((t(coefficients)))
-  names(fit$coefficients) <- c("g0", "g1", "g2", "g3", "h1", "h2", "h3")
-  fit$floor <- floor
-  return(fit)
+  if(length(rainfall) != length(t)){
+    stop("rainfall and t must be of equal length")
+  }
+  if(floor < 0){
+    stop("floor must be >= 0")
+  }
+
+  if(floor == 0){
+    fit <- fit_lm(rainfall, t)
+    out <- list(coefficients = fit$coef)
+  } else {
+    fit <- stats::optim(par = rep(0, 6), fn = objective,
+                 method = "L-BFGS-B",
+                 lower = rep(-2, 7), upper = rep(2, 7),
+                 t = t, floor = floor, rainfall = rainfall)
+    out <- list(coefficients = c(mean(rainfall), fit$par))
+  }
+
+  names(out$coefficients) <- c("g0", "g1", "g2", "g3", "h1", "h2", "h3")
+  out$floor <- floor
+  return(out)
+}
+
+#' Linear model fit
+#'
+#' @inheritParams fit_fourier
+fit_lm <- function(rainfall, t){
+  if(any(t > 365) | any (t < 1)){
+    stop("t must be between 1 and 365")
+  }
+  t <- t / 365
+  model_data <- data.frame(
+    rainfall = rainfall,
+    g1 = basecos(level = 1, t = t),
+    g2 = basecos(level = 2, t = t),
+    g3 = basecos(level = 3, t = t),
+    h1 = basesin(level = 1, t = t),
+    h2 = basesin(level = 2, t = t),
+    h3 = basesin(level = 3, t = t))
+
+  model <- stats::lm(rainfall ~ g1 + g2 + g3 + h1 + h2 + h3, data = model_data)
+  return(model)
 }
 
 #' Cos component of fourier series
 #'
 #' @param level level: 1, 2, or 3
 #' @param t time
-basecos <- function(level, t = 1:365 / 365){
+basecos <- function(level, t){
   cos(2 * pi * t * level)
 }
 
@@ -60,6 +100,6 @@ basecos <- function(level, t = 1:365 / 365){
 #'
 #' @param level level: 1, 2, or 3
 #' @param t time
-basesin <- function(level, t = 1:365 / 365){
+basesin <- function(level, t){
   sin(2 * pi * t * level)
 }
--- tests/testthat/test-fourier-model.R
+++ tests/testthat/test-fourier-model.R
@@ -1,16 +1,20 @@
 test_that("fourier model", {
-  m1 <- fit_fourier(cos(2 * pi * 1:365 / 365), 1:365 / 365, floor = 0)
+  m1 <- fit_fourier(cos(2 * pi * 1:365 / 365), 1:365, floor = 0)
   expect_equal(class(m1), "list")
   expect_identical(round(m1$coefficients, 2), c(g0 = 0, g1 = 1, g2 = 0, g3 = 0, h1 = 0, h2 = 0, h3 = 0))
   expect_identical(m1$floor, 0)
 
-  m2 <- fit_fourier(sin(2 * pi * 1:365 / 365), 1:365 / 365, floor = 0)
+  m2 <- fit_fourier(sin(2 * pi * 1:365 / 365), 1:365, floor = 0)
   expect_equal(class(m2), "list")
   expect_identical(round(m2$coefficients, 2), c(g0 = 0, g1 = 0, g2 = 0, g3 = 0, h1 = 1, h2 = 0, h3 = 0))
   expect_identical(m2$floor, 0)
 
-  m3 <- fit_fourier(sin(2 * pi * 1:365 / 365), 1:365 / 365, floor = 10)
+  m3 <- fit_fourier(sin(2 * pi * 1:365 / 365), 1:365, floor = 10)
   expect_identical(m3$floor, 10)
-  pred <- fourier_predict(m3$coefficients, t = 1:365 / 365, floor = m3$floor)
+  pred <- fourier_predict(m3$coefficients, t = 1:365, floor = m3$floor)
   expect_equal(min(pred$profile), 10)
+
+  expect_error(fit_fourier(1:10, 1:9), "rainfall and t must be of equal length")
+  expect_error(fit_fourier(1:10, 1:10, floor = -1), "floor must be >= 0")
+  expect_error(fourier_predict(coef = rep(0, 7), t = 366, floor = 0), "t must be between 1 and 365")
 })
